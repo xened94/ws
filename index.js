2b216e330505fec825e564ed81319ab6cb1b1b4f
Jonathan <xened37@gmail.com>
2023-10-06 16:12:51 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const WebSocket = require('ws');
+var wss= new WebSocket.Server({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+});
